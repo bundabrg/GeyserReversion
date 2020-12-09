3cbc269c201555131192ab49e3401ec8ad1b890e
bundabrg <bundabrg@grieve.com.au>
2020-12-09 11:38:38 +0800

    Fix for Geyser PR1640
    References: https://github.com/GeyserMC/Geyser/pull/1640

--- geyser-reversion/src/main/java/au/com/grieve/geyser/reversion/server/GeyserUpstreamPacketHandler.java
+++ geyser-reversion/src/main/java/au/com/grieve/geyser/reversion/server/GeyserUpstreamPacketHandler.java
@@ -124,7 +124,7 @@ public class GeyserUpstreamPacketHandler extends UpstreamPacketHandler {
         ResourcePacksInfoPacket resourcePacksInfo = new ResourcePacksInfoPacket();
         for (ResourcePack resourcePack : ResourcePack.PACKS.values()) {
             ResourcePackManifest.Header header = resourcePack.getManifest().getHeader();
-            resourcePacksInfo.getResourcePackInfos().add(new ResourcePacksInfoPacket.Entry(header.getUuid().toString(), header.getVersionString(), resourcePack.getFile().length(), "", "", "", false));
+            resourcePacksInfo.getResourcePackInfos().add(new ResourcePacksInfoPacket.Entry(header.getUuid().toString(), header.getVersionString(), resourcePack.getFile().length(), "", "", "", false, false));
         }
         resourcePacksInfo.setForcedToAccept(GeyserConnector.getInstance().getConfig().isForceResourcePacks());
         session.sendUpstreamPacket(resourcePacksInfo);
